c8721fd4a386db6c01a47c1bdbce29521baa6129
ArthurHutchinson <arthur.y.hutchinson@gmail.com>
2025-11-07 18:05:28 -0500

    Fixed DayZ Suit's Directors, Fixed Beril Suit Headgear Not Equipping

--- Scripts/config.cpp
+++ Scripts/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {""};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"DZ_Data", "DZ_Scripts", "DZ_Characters", "SFM", "SFM_2", "SFMComplete"};
+		requiredAddons[] = {"DZ_Data", "DZ_Scripts", "DZ_Characters", "SFM", "SFM_2", "SFM_Complete"};
 	};
 };
 class CfgMods
@@ -13,7 +13,7 @@ class CfgMods
     class DayZ_Suit_Script_Mod
     {
         name="Suit Script";
-        dir="DayZ_Suit_Script";
+        dir="SEOZ_DayZ_Suit_Script";
         picture="";
         action="";
         author="Spaghetti-Bit";
@@ -36,21 +36,21 @@ class CfgMods
 
 			class engineScriptModule 
 			{ 
-				files[] = { "DayZ_Suit_Script/Scripts/1_Core"};
+				files[] = { "SEOZ_DayZ_Suit_Script/Scripts/1_Core"};
 			};
 
 			class gameScriptModule
 			{
-				files[] = { "DayZ_Suit_Script/Scripts/3_Game" };
+				files[] = { "SEOZ_DayZ_Suit_Script/Scripts/3_Game" };
 			};
 			class worldScriptModule
 			{
-				files[] = { "DayZ_Suit_Script/Scripts/4_World" };
+				files[] = { "SEOZ_DayZ_Suit_Script/Scripts/4_World" };
 			};
 
 			class missionScriptModule 
 			{
-				files[] = { "DayZ_Suit_Script/Scripts/5_Mission" };
+				files[] = { "SEOZ_DayZ_Suit_Script/Scripts/5_Mission" };
 			};
 		};
     };
@@ -60,12 +60,25 @@ class CfgVehicles
 {
 	// TODO: Add BULAT SUIT (SKAT SUIT)
 		// FOR BULAT SUIT USE SEVA SUIT NOT PS5M
+	// TODO: Organize by suit type, not faction.
+
+	class SFM_SunriseVestFull_1;
+	class SFM_SevaVestFull;
+	class SFM_BerilVest_Stalker;
+	class SFM_VestMerc_Stalker;
+	class SFM_SSP99M_Back_Red;
+	class SFM_SSP99M_Back_Green;
+	class SFMC_SunriseVestFull_Duty_1;
+	class SFM_SevaVestFull_Dolg1;
+	class SFM_SevaVestFull_Svoboda1;
+	class SFM_BerilVest_Svoboda1;
+	class SFM_BerilVest_Base;
+	class SFM_VestMerc_Bandit;
 
 	  ///////////
 	 // LONER //
 	///////////
 
-	class SFM_SunriseVestFull_1;
 	class SFM_Suit_SunriseFull_1: SFM_SunriseVestFull_1
 	{
 		scope = 2;
@@ -81,7 +94,6 @@ class CfgVehicles
 		suitSlots[] = {"Body", "Legs","Back"};
 	};
 
-	class SFM_SevaVestFull;
 	class SFM_Suit_SevaFull: SFM_SevaVestFull
 	{
 		scope = 2;
@@ -90,33 +102,31 @@ class CfgVehicles
 		suitSlots[] = {"Body","Legs","Feet","Gloves","Mask","Back"};
 	};
 
-	
-	class SFM_BerilVest_Stalker;
-	class SFM_Suit_Beril_Stalker: SFM_BerilVest_Stalker
-	{
-		// Had to use SFM_BerilJacket_Base because it uses the STALKER texture anyway. If it's bad, needs a new texture.
-		scope = 2;
-		displayName = "Berill Suit (Loner)";
-		suit[] = {"SFM_BerilJacket_Base","SFM_BerilPants_Stalker","SFM_BerilHelmet_Stalker","SFM_GasMask_XM40"};
-		suitSlots[] = {"Body","Legs","Head","Mask"};
-	};
+	// TODO: Make the shirt using Berill_shirt_stalker.paa
+	// class SFM_Suit_Beril_Stalker: SFM_BerilVest_Stalker
+	// {
+	// 	// Had to use SFM_BerilJacket_Base because it uses the STALKER texture anyway. If it's bad, needs a new texture.
+	// 	scope = 2;
+	// 	displayName = "Berill Suit (Loner)";
+	// 	suit[] = {"SFM_BerilJacket_Base","SFM_BerilPants_Stalker","SFM_BerilHelmet_Stalker","SFM_GasMask_XM40"};
+	// 	suitSlots[] = {"Body","Legs","Headgear","Mask"};
+	// };
 
 	// TODO: Make suit Item for Loner Merc
-	class SFM_VestMerc_Stalker;
-	class SFM_Suit_Loner_Merc: SFM_VestMerc_Stalker
-	{
-		scope = 2;
-		displayName = "Merc Suit (Loner)";
-		suit[] = {"SFM_MercJacket_neutral","SFM_MercPants_Stalker"};
-		// TODO: Merc Suit Stalker needs Pants.
-		suitSlots[] = {"Body","Legs"};
-	}
+	// class SFM_Suit_Loner_Merc: SFM_VestMerc_Stalker
+	// {
+	// 	scope = 2;
+	// 	displayName = "Merc Suit (Loner)";
+	// 	suit[] = {"SFM_MercJacket_neutral","SFM_MercPants_Stalker"};
+	// 	// TODO: Merc Suit Stalker needs Pants.
+	// 	suitSlots[] = {"Body","Legs"};
+	// };
 
 	  ///////////////
 	 // ECOLOGIST //
 	///////////////
 
-	class SFM_SSP99M_Back_Red;
+
 	class SFM_Suit_SSP99M_Red: SFM_SSP99M_Back_Red
 	{
 		// TODO: Add in the null/invisible slot for the 'vest' (NH should have one). Reason why is because when using the suit vests above will dupe the pants/jacket and drop it on the ground.
@@ -127,7 +137,7 @@ class CfgVehicles
 	};
 
 	// TODO: Needs to be tested.
-	class SFM_SSP99M_Back_Green;
+	
 	class SFM_Suit_SSP99M_Green: SFM_SSP99M_Back_Green
 	{
 		// TODO: Add in the null/invisible slot for the 'vest' (NH should have one). Reason why is because when using the suit vests above will dupe the pants/jacket and drop it on the ground.
@@ -144,8 +154,7 @@ class CfgVehicles
 	 // DUTY //
 	//////////
 
-	class SFMC_SunriseVestFull_Duty_1;
-	class SFMC_Suit_Duty_Sunrise: SFMC_SunriseVestFull_Duty_1
+	class SFM_Suit_Duty_Sunrise: SFMC_SunriseVestFull_Duty_1
 	{
 		scope = 2;
 		displayName = "Sunrise Suit (Duty)";
@@ -153,7 +162,7 @@ class CfgVehicles
 		suitSlots[] = {"Body", "Legs"};
 	};
 	
-	class SFMC_Suit_Duty_Sunrise_Gas: SFMC_SunriseVestFull_Duty_1
+	class SFM_Suit_Duty_Sunrise_Gas: SFMC_SunriseVestFull_Duty_1
 	{
 		scope = 2;
 		displayName = "Sunrise Suit (Duty) (Gas Tank)";
@@ -161,7 +170,6 @@ class CfgVehicles
 		suitSlots[] = {"Body", "Legs","Back"};
 	};
 
-	class SFM_SevaVestFull_Dolg1;
 	class SFM_Suit_Duty_Seva: SFM_SevaVestFull_Dolg1
 	{
 		scope = 2;
@@ -181,7 +189,6 @@ class CfgVehicles
 	// TODO: Make Suit Item for Freedom Sunrise
 		// TODO: Make the invidiual items for Freedom Sunrise
 
-	class SFM_SevaVestFull_Svoboda1;
 	class SFM_Suit_Freedom_Seva: SFM_SevaVestFull_Svoboda1
 	{
 		scope = 2;
@@ -191,13 +198,12 @@ class CfgVehicles
 	};
 
 	// TODO: Make Suit Item for Freedom Beril
-	class SFM_BerilVest_Svoboda1;
 	class SFM_Suit_Freedom_Beril: SFM_BerilVest_Svoboda1
 	{
 		scope = 2;
 		displayName = "Berill Suit (Freedom)";
 		suit[] = {"SFM_BerilJacket_Svoboda1","SFM_BerilPants_Svoboda1","SFM_BerilHelmet_Svoboda1","SFM_GasMask_XM40"};
-		suitSlots[] = {"Body","Legs","Head","Mask"};
+		suitSlots[] = {"Body","Legs","Headgear","Mask"};
 	};
 
 	// TODO: Make Suit Item for Freedom Merc
@@ -218,13 +224,13 @@ class CfgVehicles
 	//////////////
 
 	// TODO: TEST
-	class SFM_BerilVest_Base;
+
 	class SFM_Suit_Military_Beril: SFM_BerilVest_Base
 	{
 		scope = 2;
 		displayName = "Berill Suit (Military)";
-		suit[] = {"SFM_BerilJacket_Voen1","SFM_BerilPants_Base","SFM_BerilHelmet_Base","SFM_GasMask_XM40"};
-		suitSlots[] = {"Body","Legs","Head","Mask"};
+		suit[] = {"SFM_BerilJacket_Voen1","SFM_BerilPants_Voen1","SFM_BerilHelmet_Voen1","SFM_GasMask_XM40"};
+		suitSlots[] = {"Body","Legs","Headgear","Mask"};
 	};
 
 	  ////////////
@@ -232,14 +238,13 @@ class CfgVehicles
 	////////////
 
 	// TODO: TEST
-	class SFM_VestMerc_Bandit;
-	class SFM_Suit_Bandit_Merc: SFM_VestMerc_Bandit
-	{
-		scope = 2;
-		displayName = "Merc Suit (Bandit)";
-		suit[] = {"SFM_MercJacket_bandit","SFMC_MercPants_Bandit"};
-		suitSlots[] = {"Body","Legs"};
-	};
+	// class SFM_Suit_Bandit_Merc: SFM_VestMerc_Bandit
+	// {
+	// 	scope = 2;
+	// 	displayName = "Merc Suit (Bandit)";
+	// 	suit[] = {"SFM_MercJacket_bandit","SFMC_MercPants_Bandit"};
+	// 	suitSlots[] = {"Body","Legs"};
+	// };
 
 
 	// smf_wfvest_green
